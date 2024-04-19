34ced6bc7d55b54514022e627ae4fec3d0b3da4a
poonamk3 <poonamk@thoughtwin.com>
2024-04-19 12:05:44 +0530

    change debug

--- notes/settings.py
+++ notes/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zt8*frb&#-fs&qe&x+qr!*z@9$ig)3w!^d#uie-q56@$ny^ap$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
