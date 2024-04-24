f774ae07d653f9cb4247e8496a8b889eb2c928e1
poonamk3 <poonamk@thoughtwin.com>
2024-04-24 16:10:27 +0530

    add

--- notes/settings.py
+++ notes/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-zt8*frb&#-fs&qe&x+qr!*z@9$ig)3w!^d#uie-q56@$ny^ap$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000"
